7dc0cbbd3e7bdfb3073bf9cac9da6ef5e6dec1b3
parsarback <par.sarback@gmail.com>
2019-07-09 04:30:12 +0200

    Enabled Swagger

--- sustaina-services-app/app/com/sustainasearch/services/catalog/CatalogController.scala
+++ sustaina-services-app/app/com/sustainasearch/services/catalog/CatalogController.scala
@@ -1,18 +1,18 @@
 package com.sustainasearch.services.catalog
 
-//import io.swagger.annotations.{Api, ApiOperation}
+import io.swagger.annotations.{Api, ApiOperation}
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{Action, Controller}
 
 @Singleton
-//@Api(value = "/catalog")
+@Api(value = "/catalog")
 class CatalogController @Inject()() extends Controller {
 
-//  @ApiOperation(
-//    httpMethod = "GET",
-//    value = "Searches the SustainaCatalog",
-//    produces = "application/json"
-//  )
+  @ApiOperation(
+    httpMethod = "GET",
+    value = "Searches the SustainaCatalog",
+    produces = "application/json"
+  )
   def search() = Action { implicit request =>
     Ok("Search result from the SustainaCatalog")
   }
