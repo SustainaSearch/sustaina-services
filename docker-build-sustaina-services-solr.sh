35a5510f05183b36ddabecff1b03253e52c5913c
parsarback <par.sarback@gmail.com>
2019-09-13 17:51:36 +0200

    Renamed module to sustaina-services-solr-cores

--- docker-build-sustaina-services-solr.sh
+++ docker-build-sustaina-services-solr.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t sustaina-services-solr sustaina-services-catalog/sustaina-services-products-solr-core
+docker build -t sustaina-services-solr sustaina-services-catalog/sustaina-services-solr-cores
