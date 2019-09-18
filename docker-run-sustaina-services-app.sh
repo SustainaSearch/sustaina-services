f839fcfccb0f2accbcdc696930e4fb705e5d3a14
parsarback <par.sarback@gmail.com>
2019-09-18 10:18:49 +0200

    Renamed variable to SOLR_HOST

--- docker-run-sustaina-services-app.sh
+++ docker-run-sustaina-services-app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Gets the gateway of the running sustaina-services-solr container and strips the leading and trailing quotes
-HOST=$(docker inspect sustaina-services-solr | jq '.[].NetworkSettings.Networks.bridge.Gateway' | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
-SOLR_BASE_URL="http://$HOST:8983"
+SOLR_HOST=$(docker inspect sustaina-services-solr | jq '.[].NetworkSettings.Networks.bridge.Gateway' | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
+SOLR_BASE_URL="http://$SOLR_HOST:8983"
 echo 'SOLR_BASE_URL = ' $SOLR_BASE_URL
 docker run -it -p 9000:9000  -e "JAVA_OPTS=-Dproducts.solr.url=$SOLR_BASE_URL" --rm sustaina-services-app
