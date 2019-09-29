52e593a7360e44207b9520113a85174d04be920c
parsarback <par.sarback@gmail.com>
2019-09-29 21:41:32 +0200

    Generating flow types for all API models

--- sustaina-services-app/app/com/sustainasearch/services/v1/models/FlowTypeService.scala
+++ sustaina-services-app/app/com/sustainasearch/services/v1/models/FlowTypeService.scala
@@ -2,7 +2,12 @@ package com.sustainasearch.services.v1.models
 
 import bridges.flow._
 import bridges.flow.syntax._
-import com.sustainasearch.services.v1.NameApiModel
+import com.sustainasearch.services.v1.catalog.CatalogQueryResponseApiModel
+import com.sustainasearch.services.v1.catalog.products._
+import com.sustainasearch.services.v1.catalog.products.clothes.{ClothesApiModel, CompositionApiModel}
+import com.sustainasearch.services.v1.catalog.products.facets.{BrandFacetApiModel, CategoryFacetApiModel, ProductFacetsApiModel}
+import com.sustainasearch.services.v1.catalog.products.food.{BabyFoodApiModel, IngredientStatementApiModel}
+import com.sustainasearch.services.v1.{ImageApiModel, NameApiModel}
 import javax.inject.{Inject, Singleton}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -14,7 +19,26 @@ class FlowTypeService @Inject()()(implicit ec: ExecutionContext) {
     Future(
       Flow.render(
         List(
-          decl[NameApiModel]
+          decl[ImageApiModel],
+          decl[NameApiModel],
+          decl[CatalogQueryResponseApiModel],
+          decl[CategoryApiModel],
+          decl[CityApiModel],
+          decl[CountryApiModel],
+          decl[ProductActivityApiModel],
+          decl[ProductApiModel],
+          decl[ProductContainerApiModel],
+          decl[ProductQueryResponseApiModel],
+          decl[RepresentativePointApiModel],
+          decl[SimpleProductApiModel],
+          decl[SimpleProductQueryResponseApiModel],
+          decl[ClothesApiModel],
+          decl[CompositionApiModel],
+          decl[BrandFacetApiModel],
+          decl[CategoryFacetApiModel],
+          decl[ProductFacetsApiModel],
+          decl[BabyFoodApiModel],
+          decl[IngredientStatementApiModel]
         )
       )
     )
