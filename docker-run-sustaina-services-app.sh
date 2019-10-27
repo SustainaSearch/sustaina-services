82d1cc9630888c7a3ef0ef49d3085e303858079e
parsarback <par.sarback@gmail.com>
2019-10-27 22:39:01 +0100

    Using Keychain to store AWS access and secret keys

--- docker-run-sustaina-services-app.sh
+++ docker-run-sustaina-services-app.sh
@@ -2,10 +2,15 @@
 # Gets the gateway of the running sustaina-services-solr container and strips the leading and trailing quotes
 SOLR_HOST=$(docker inspect sustaina-services-solr | jq '.[].NetworkSettings.Networks.bridge.Gateway' | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
 SOLR_BASE_URL="http://$SOLR_HOST:8983"
-echo 'SOLR_BASE_URL = ' $SOLR_BASE_URL
+echo 'SOLR_BASE_URL = '$SOLR_BASE_URL
 
-# TODO: get access key and secret key from key chain
-AWS_ACCESS_KEY="AKIAVT7LFIGAJRT4LFPA"
-AWS_SECRET_KEY=""
+# Gets the sustaina_bot AWS access key and secret key from the Keychain
+AWS_ACCESS_KEY=$(security find-generic-password -a sustaina_bot -ws aws.access.key)
+echo 'AWS_ACCESS_KEY = '"${AWS_ACCESS_KEY:0:4}****************"
+AWS_SECRET_KEY=$(security find-generic-password -a sustaina_bot -ws aws.secret.key)
+echo 'AWS_SECRET_KEY = '"${AWS_SECRET_KEY:0:4}************************************"
+
 AWS_REGION="us-east-1"
+echo 'AWS_REGION = '"$AWS_REGION"
+
 docker run -it -p 9000:9000  -e "JAVA_OPTS=-Dproducts.solr.url=$SOLR_BASE_URL -Daws.access.key=$AWS_ACCESS_KEY -Daws.secret.key=$AWS_SECRET_KEY -Daws.region=$AWS_REGION" --rm sustaina-services-app
