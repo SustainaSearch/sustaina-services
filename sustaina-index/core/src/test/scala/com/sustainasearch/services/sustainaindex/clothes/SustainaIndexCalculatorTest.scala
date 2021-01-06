8d4b4d0949ec48f3b1d6fd815f34eb2cb7f3b2e6
parsarback <par.sarback@gmail.com>
2021-01-06 15:19:59 +0100

    fixed broken test

--- sustaina-index/core/src/test/scala/com/sustainasearch/services/sustainaindex/clothes/SustainaIndexCalculatorTest.scala
+++ sustaina-index/core/src/test/scala/com/sustainasearch/services/sustainaindex/clothes/SustainaIndexCalculatorTest.scala
@@ -1,24 +1,31 @@
 package com.sustainasearch.services.sustainaindex.clothes
 
+import com.sustainasearch.services.sustainaindex.certification.InMemoryCertificationRepository
+import com.sustainasearch.services.sustainaindex.clothes.material.{Material, MaterialGroup, MaterialType}
+import com.sustainasearch.services.sustainaindex.country.Country
 import com.sustainasearch.services.sustainaindex.{SustainaIndex, Tenant}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.Success
 
 class SustainaIndexCalculatorTest extends WordSpec with Matchers with ScalaFutures {
   val underTest = new SustainaIndexCalculator()
 
   classOf[SustainaIndexCalculator].getSimpleName should {
-    "calculate sustaina index for clothes" in {
-
-	// Test country
+    "calculate sustaina index for clothes with country" in {
+      val country = Country(
+        countryCode = "SE",
+        renewableEnergy = 53.25F,
+        crc = 95.0F
+      )
       val input = SustainaIndexInput(
         tenant = Tenant(id = "1", host = "host_1"),
         item = Item(
           id = "pid1",
-          country = Option(Country.withCountryCode("SE")),
+          country = Option(country),
           certifications = Seq.empty,
           materials = Seq.empty,
           garmentWeight = None,
@@ -29,43 +36,64 @@ class SustainaIndexCalculatorTest extends WordSpec with Matchers with ScalaFutur
       whenReady(eventualResult) { result =>
         result shouldBe Success(SustainaIndex(0.2015F))
       }
+    }
 
-	  // Test certification
-	  val input = SustainaIndexInput(
-        tenant = Tenant(id = "1", host = "host_1"),
-        item = Item(
-          id = "pid1",
-          country = None,
-          certifications = Seq(Certification.withId(0), Certification.withId(1)),
-          materials = Seq.empty,
-          garmentWeight = None,
-          brand = None
+    "calculate sustaina index for clothes with certification" in {
+      val certificationRepository = new InMemoryCertificationRepository()
+      val cert0F = certificationRepository.getCertification(0).map(_.get)
+      val cert1F = certificationRepository.getCertification(1).map(_.get)
+      val certsF = Future.sequence(Seq(cert0F, cert1F))
+
+      whenReady(certsF) { certs =>
+        val input = SustainaIndexInput(
+          tenant = Tenant(id = "1", host = "host_1"),
+          item = Item(
+            id = "pid1",
+            country = None,
+            certifications = certs,
+            materials = Seq.empty,
+            garmentWeight = None,
+            brand = None
+          )
         )
-      )
-      val eventualResult = underTest.calculateSustainaIndex(input)
-      whenReady(eventualResult) { result =>
-        result shouldBe Success(SustainaIndex(0.21F))
+        val eventualResult = underTest.calculateSustainaIndex(input)
+        whenReady(eventualResult) { result =>
+          result shouldBe Success(SustainaIndex(0.21F))
+        }
       }
+    }
 
-	// Test certification
-	  val input = SustainaIndexInput(
+    "calculate sustaina index for clothes with material" in {
+      val material = Material(
+        materialType = MaterialType(
+          id = 1,
+          group = MaterialGroup(
+            id = 10,
+            score = 100F
+          )
+        ),
+        percent = 100F
+      )
+      val input = SustainaIndexInput(
         tenant = Tenant(id = "1", host = "host_1"),
         item = Item(
           id = "pid1",
           country = None,
           certifications = Seq.empty,
-          materials = Seq(Material(MaterialType(1), 1.0F)),
+          materials = Seq(material),
           garmentWeight = None,
           brand = None
         )
       )
       val eventualResult = underTest.calculateSustainaIndex(input)
       whenReady(eventualResult) { result =>
-        result shouldBe Success(SustainaIndex(0.4F))
+        result shouldBe Success(SustainaIndex(0.39999998F))
       }
 
-	  // Test empty
-	  val input = SustainaIndexInput(
+    }
+
+    "calculate sustaina index for clothes with empty input" in {
+      val input = SustainaIndexInput(
         tenant = Tenant(id = "1", host = "host_1"),
         item = Item(
           id = "pid1",
