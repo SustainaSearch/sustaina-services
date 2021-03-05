0154284259ef18ee7c346172769d6027e6f21f56
parsarback <par.sarback@gmail.com>
2021-03-05 14:06:23 +0100

    Updated broken test

--- sustaina-index/core/src/test/scala/com/sustainasearch/services/sustainaindex/clothes/material/InMemoryMaterialTypeStorageTest.scala
+++ sustaina-index/core/src/test/scala/com/sustainasearch/services/sustainaindex/clothes/material/InMemoryMaterialTypeStorageTest.scala
@@ -6,13 +6,13 @@ class InMemoryMaterialTypeStorageTest extends WordSpec with Matchers {
 
   "InMemoryMaterialTypeStorage" should {
     "find MaterialTypeStorage.Val with id" in {
-      MaterialTypeStorage(0) shouldBe MaterialTypeStorage.Cotton
-      MaterialTypeStorage(1) shouldBe MaterialTypeStorage.Wool
+      MaterialTypeStorage(0) shouldBe MaterialTypeStorage.Cotton_NoInfo
+      MaterialTypeStorage(14) shouldBe MaterialTypeStorage.Wool_NonChlorine
     }
 
     "provide group" in {
-      MaterialTypeStorage.Cotton.group shouldBe MaterialGroupStorage.group0
-      MaterialTypeStorage.Wool.group shouldBe MaterialGroupStorage.group2
+      MaterialTypeStorage.Cotton_NoInfo.group shouldBe MaterialGroupStorage.group1
+      MaterialTypeStorage.Wool_NonChlorine.group shouldBe MaterialGroupStorage.group3
     }
   }
 }
